b11e55a38bebc56caf0ce400758e175e4ecba8b6
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-08-14 15:22:55 -0600

    fix: test navbar

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,6 +1,6 @@
 import './landing.css'
 import React from 'react';
-import {Navbar} from './components/NavBar/navbar.jsx'
+import Navbar from './components/NavBar/navbar.jsx'
 
 
 //Aquí se añadira la estructura de nelly (recordar que esto nos HTML es JSX)
