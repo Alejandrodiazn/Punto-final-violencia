c8a17611c33ca8765b2152b5781ceadc7a0b0823
Karlo Paz <kpaz30@alumnos.uaq.mx>
2023-08-18 13:08:00 -0600

    feat: Creacion de modulo Router y pagina de conversacion 1. Modificación del archivo vite.config

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,17 +1,16 @@
-import './landing.css'
-import React from 'react';
-import {Navbar} from './components/NavBar/navbar.jsx'
-
+import "./Landing.css";
+import React from "react";
+import { Navbar } from "./components/NavBar/navbar.jsx";
 
 //Aquí se añadira la estructura de nelly (recordar que esto nos HTML es JSX)
 function Landing() {
-return (
-    <main>
-        <Navbar/>
-        <div>
-            <p>Hola soy la landing</p>
-        </div>
-    </main>
-    )
+  return (
+    <>
+      <Navbar />
+      <div>
+        <p>Hola soy la landing</p>
+      </div>
+    </>
+  );
 }
-export {Landing}
+export { Landing };
