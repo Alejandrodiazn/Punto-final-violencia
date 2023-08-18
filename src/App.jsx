71d975125e2874336585a066da9a3354dfe1b74f
Alejandra María Cruz Flores <114045083+alejandracflores@users.noreply.github.com>
2023-08-18 13:51:51 -0600

    Document: Transladé todo el componente landing del Ruth App al page landing

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,7 @@ function App() {
   return (
     <main>
       <Landing/>
-      <div className="inicio">
-        <div className="fondo">
-          <div className="carousel">
-            <div className="slide"></div>
-            <img className="logo" src='src/images/logo.png'></img>
-            <div className='cajitadenombres'>
-              <div className="name1"><p>Punto final</p></div>
-              <div className="name2"><p>a la violencia</p></div>
-            </div>
-            <img className="flechita" src='src/images/flechita.png'></img>
-          </div>
-        </div>
-      </div>
+      
     </main>
   )
 }
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -11,6 +11,19 @@ return (
         {/* <div>
             <p>Hola soy la landing</p>
         </div> */}
+        <div className="inicio">
+        <div className="fondo">
+          <div className="carousel">
+            <div className="slide"></div>
+            <img className="logo" src='src\pages\landing\images\logo.png'></img>
+            <div className='cajitadenombres'>
+              <div className="name1"><p>Punto final</p></div>
+              <div className="name2"><p>a la violencia</p></div>
+            </div>
+            <img className="flechita" src='src\pages\landing\images\flechita.png'></img>
+          </div>
+        </div>
+      </div>
     </main>
     )
 }
