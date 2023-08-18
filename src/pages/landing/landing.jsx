d44c8ab2643474c7eef2c0d002b8e2d16169f7c9
J Antonio Herrera <jherrera51@alumnos.uaq.mx>
2023-08-18 13:08:04 -0600

    fix: mobile navbar finished

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -5,13 +5,10 @@ import Navbar from './components/NavBar/navbar.jsx'
 
 //Aquí se añadira la estructura de nelly (recordar que esto nos HTML es JSX)
 function Landing() {
-  return (
-    <>
-      <Navbar />
-      <div>
-        <p>Hola soy la landing</p>
-      </div>
-    </>
-  );
+return (
+    <main>
+        <Navbar/>
+    </main>
+    )
 }
 export { Landing };
