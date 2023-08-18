fddbb5635a21c9689fb8a15c743610a56c4f85d0
Karlo Paz <kpaz30@alumnos.uaq.mx>
2023-08-18 14:45:30 -0600

    fix: Commit supremo arreglador de directorios, si dios conmigo, nadie contra mi.

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,30 +1,39 @@
-import './landing.css'
-import React from 'react';
-import Navbar from './components/NavBar/navbar.jsx'
-
+import "./landing.css";
+import React from "react";
+import Navbar from "../components/NavBar/navbar.jsx";
 
 //Aquí se añadira la estructura de nelly (recordar que esto no es HTML es JSX)
 function Landing() {
-return (
+  return (
     <main>
-        <Navbar/>
+        <Navbar />
         {/* <div>
             <p>Hola soy la landing</p>
         </div> */}
         <div className="inicio">
-        <div className="fondo">
-          <div className="carousel">
-            <div className="slide"></div>
-            <img className="logo" src='src\pages\landing\images\logo.png'></img>
-            <div className='cajitadenombres'>
-              <div className="name1"><p>Punto final</p></div>
-              <div className="name2"><p>a la violencia</p></div>
+          <div className="fondo">
+            <div className="carousel">
+              <div className="slide"></div>
+              <img
+                className="logo"
+                src="src\pages\landing\images\logo.png"
+              ></img>
+              <div className="cajitadenombres">
+                <div className="name1">
+                  <p>Punto final</p>
+                </div>
+                <div className="name2">
+                  <p>a la violencia</p>
+                </div>
+              </div>
+              <img
+                className="flechita"
+                src="src\pages\landing\images\flechita.png"
+              ></img>
             </div>
-            <img className="flechita" src='src\pages\landing\images\flechita.png'></img>
           </div>
-        </div>
       </div>
     </main>
-    )
+  );
 }
 export { Landing };
