0702b8faf19347e043f03f7cce0b871955795e23
Frida0M <folivera10@gmail.com>
2023-08-23 15:39:02 -0600

    homescreen add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Landing } from "./pages/landing/landing";
 import { Conversation } from "./pages/conversation/Conversation";
+import { HomeScreen } from "./pages/homescreen/homescreen";
 
 //AQUI SE CORREGIRÁ TODO SE AÑADIRAN ROOTERS PARA MOVERSE ENTRE PAGINAS
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route index element={<Landing />}/>
         <Route path="/Conversacion-1" element={<Conversation/>}/>
+        <Route path="/HomeScreen" element={<HomeScreen/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/homescreen/homescreen.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function HomeScreen() {
+    console.log("Hola");
+  return <h1>Hola desde mi componente JSX</h1>;
+}
+
+export {HomeScreen};
