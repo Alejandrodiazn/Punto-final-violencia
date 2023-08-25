06254766daa032696f76fe7ba3f4d9297feee88f
Sack Valdez <rauldaxo@gmail.com>
2023-08-24 22:45:13 -0600

    agregado de pantalla celular, hook de tiempo real, statusBar y archivos css

---
+++ src/pages/homescreen/StatusBar.jsx
@@ -0,0 +1,18 @@
+// StatusBar.jsx
+
+import React from 'react';
+import './statusbar.css';
+
+function StatusBar() {
+  return (
+    <div className="status-bar">
+      <div className="signal"></div>
+      <div className="battery">
+        <div className="battery-icon"></div>
+        <div className="battery-percentage">75%</div>
+      </div>
+    </div>
+  );
+}
+
+export default StatusBar;
--- src/pages/homescreen/homescreen.jsx
+++ src/pages/homescreen/homescreen.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import StatusBar from './StatusBar';
+import './homescreen.css';
+
 
 function HomeScreen() {
-    console.log("Hola");
+ 
+  // Hook para reloj
+  useEffect(() => { 
+    const updateTime = () => {
+      const now = new Date();
+      const timeElement = document.getElementById('time');
+      if (timeElement) {
+        timeElement.textContent = now.toLocaleTimeString();
+      }
+    };
+    updateTime();
+    const interval = setInterval(updateTime, 1000); // Actualizar cada segundo
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-        <h2>Esta es la hora</h2>
+    <div className="container">
+      <div className="box">
+      <div className="statusBar"> <StatusBar /> </div>
+      
+        <h2>MANSPLAINING</h2>
+        {/* Real-time clock */}
+        <div className="clock">
+          <p id="time"></p>
+        </div>
+
+        <div className="buttons">
         <button>Wunsup</button>
         <button>Recursos</button>
         <button>Salir</button>
         <button>Nosotros</button>
         <button>FAQ</button>
         <button>Ayuda</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export {HomeScreen};
+export { HomeScreen };
+
